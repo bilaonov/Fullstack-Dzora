d377eb378d99efa40f0f99a1925bba0c49cceebd
Amirkhan15reg <75429457+Amirkhan15reg@users.noreply.github.com>
2021-09-10 01:03:42 +0300

    graphql resolvers word scheme

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,63 @@
+const {ApolloServer, gql} = require('apollo-server-express')
 import express from 'express'
 
 const app = express()
 const port = 5000
+
+const typeDefs = gql`
+    type Query {
+        words: [Word!]!
+    }
+
+    type Word {
+        id: ID
+        word: String
+        translates: [Translates]
+    }
+
+    type Translates {
+        id: ID
+        word: String
+        language: String
+        word_id: Int
+    }
+`;
+
+const resolvers = {
+    Query: {
+        words: () => words
+    },
+    Word: {
+        translates: (word: any) => {
+            return translate.filter((translates) => translates.id === word.id)
+        }
+    }
+
+}
+
+let words = [{
+    id: '1',
+    word: 'Хуцау'
+}]
+
+let translate = [{
+    id: '1',
+    word: 'Аллах',
+    language: 'rus',
+    word_id: 1
+}]
+
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+server.applyMiddleware({app, path: '/api'})
 
 
 app.get('/', (request, response) => {
     response.send("hello world")
 })
 
-app.listen(port, () => console.log(`Сервер запущен на ${port} порту`))
+app.listen({ port }, () =>
+    console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+)
+);
